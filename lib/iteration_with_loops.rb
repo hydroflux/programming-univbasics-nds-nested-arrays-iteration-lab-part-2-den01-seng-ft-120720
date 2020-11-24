71cc99a55d643068df516e86efdaa49b95ca5d14
Jack Hubert <jphubert93@gmail.com>
2020-11-24 22:15:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,18 +9,16 @@ def find_min_in_nested_arrays(src)
   while row_index < src.length do
     column_index = 0
     daily_low = 1000
-    
-    binding.pry
-    
+
     while column_index < src[row_index].length do
       if src[row_index][column_index] < daily_low
         daily_low = src[row_index][column_index]
       end
       column_index += 1
     end
+    puts daily_low
     low_temperatures << daily_low
     row_index += 1
   end
   low_temperatures
-  binding.pry
 end
